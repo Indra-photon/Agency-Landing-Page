43d78adea35589a9ad1c97c1819e099c3b14ad71
Indranil Maiti <indranilmaiti1@gmail.com>
2025-11-18 19:36:08 +0100

    set-up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        pathname: "/account123/**",
+      },
+      {
+        protocol: "https",
+        hostname: "aceternity.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import Image from 'next/image'
+import { motion } from "motion/react"
+
+function HeroSection() {
+  return (
+    <div className='flex flex-col'>
+
+        <div className='flex items-center justify-end relative max-h-screen overflow-hidden'>
+            <div className='z-10 absolute left-20'>
+                <h1 className="text-2xl md:text-7xl text-white dark:text-white bg-transparent bg-clip-text">
+                    The Ultimate <br /> <span className='italic'>development</span> studio
+                </h1>
+                <p className="max-w-2xl text-base md:text-xl mt-8 text-neutral-200 dark:text-neutral-200">
+                    We build beautiful products with the latest technologies and frameworks.
+                    We are a team of passionate developers and designers that love to build
+                    amazing products.
+                </p>
+            </div>
+            <div className='flex flex-row gap-5 rotate-x-50 rotate-z-45'>
+                <motion.div
+                initial={{ y: 500 }}
+                animate={{ y: [-1000, 1000] }}
+                transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+                 className='flex flex-col space-y-10 mb-20 opacity-30'>
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/cursor.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/rogue.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/rogue.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                </motion.div>
+
+                <motion.div
+                initial={{ y: -500 }}
+                animate={{ y: [1000, -1000] }}
+                transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+                 className='flex flex-col space-y-10 mb-20 opacity-30'>
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/cursor.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/rogue.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/rogue.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                </motion.div>
+
+                <motion.div
+                initial={{ y: 500 }}
+                animate={{ y: [-1000, 1000] }}
+                transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+                className='flex flex-col space-y-10 mb-20 opacity-30'>
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/cursor.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/rogue.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                    <Image
+                        src="https://aceternity.com/images/products/thumbnails/new/cursor.png"
+                        alt="Hero Image"
+                        width={600}
+                        height={400}
+                    />
+                </motion.div>
+
+            </div>
+        </div>
+
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
